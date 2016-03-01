834d3d9107f3b5551ae84386d4bb895ae0389325
tonitiffy <toniannfitzgerald@gmail.com>
2016-03-01 02:37:42 +0000

    took out header check

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+import logging
+import sys
+
+
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 db = SQLAlchemy(app)
 from app import views, models
