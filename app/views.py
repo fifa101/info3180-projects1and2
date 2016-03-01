2f95701b2555b4ae68a45a7f817fc2167f241bbe
tonitiffy <toniannfitzgerald@gmail.com>
2016-03-01 02:55:13 +0000

    added route to send image

--- app/views.py
+++ app/views.py
@@ -88,7 +88,7 @@ def view_profile(userid):
 # The functions below should be applicable to all Flask apps.
 ###
 
-@app.route('/<file_name>')
+@app.route('/profile/<userid>/<file_name>')
 def send_img_file(file_name):
     """Send your static image file."""
     return app.send_static_file(file_name)
